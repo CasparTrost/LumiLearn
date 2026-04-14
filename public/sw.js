1f6ef86137bd8b3977f343622b9e83da4fdc8b7b
Caspar Trost <51917955+CasparTrost@users.noreply.github.com>
2026-04-14 09:57:39 +0200

    fix: sw.js - auto-unregister

--- public/sw.js
+++ public/sw.js
@@ -1,39 +1,9 @@
-// Service Worker: always fetch index.html fresh, cache everything else
-const CACHE = 'lumilearn-v2'
-
-self.addEventListener('install', e => {
-  self.skipWaiting()
-})
-
+// Unregister this service worker immediately
+self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  )
-})
-
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url)
-  // index.html: always network first, never cache
-  if (url.pathname.endsWith('/') || url.pathname.endsWith('index.html')) {
-    e.respondWith(
-      fetch(e.request, { cache: 'no-store' })
-        .catch(() => caches.match(e.request))
-    )
-    return
-  }
-  // JS/CSS/assets: cache first (they have content hashes)
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached
-      return fetch(e.request).then(res => {
-        if (res.ok) {
-          const clone = res.clone()
-          caches.open(CACHE).then(c => c.put(e.request, clone))
-        }
-        return res
-      })
+    self.registration.unregister().then(() => self.clients.matchAll()).then(clients => {
+      clients.forEach(c => c.navigate(c.url))
     })
   )
 })
